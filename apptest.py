ead879003a54141f147ec6213a2753bf398f2c81
herrmann12 <163855525+herrmann12@users.noreply.github.com>
2025-06-06 16:42:03 +0200

    Update apptest.py
    Change of index name

--- apptest.py
+++ apptest.py
@@ -117,7 +117,7 @@ Is there anything else I can help you with on PlanQK?
                 "parameters": {
                 "filter": None, # filter to limit the search results (e.g., "category eq 'news'")
                 "endpoint": search_endpoint,
-                "index_name": "vector-1747684712810c",
+                "index_name": "rag-1749220504930",
                 "semantic_configuration": "",
                 "authentication": {
                     "type": "api_key",
