37797efc09ac18124f69164b1945810468dc1423
herrmann12 <ds-erman@outlook.de>
2025-09-03 16:05:02 +0200

    change from content to chunk in extra body

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def post_request(messages, user_profile=None):
                                 "strictness": 1, # strictness of the search results (0-5). 0 means no strictness, 5 means very strict. The stricter the search, the more relevant the results are. The strictness has to be between 1 and 5, where 1 allows a greater variety in answers with more data being seen as possibly relevant
                                 "top_n_documents": 10, # number of documents to retrieve from the search (1-10). The more documents are retrieved, the more relevant the results are. The top_n_documents has to be between 1 and 10, where 1 means only one document is retrieved and 10 means all documents are retrieved.
                                 "fields_mapping": {
-                                    "content_fields": ["content"],
+                                    "content_fields": ["chunk"],
                                     "vector_fields": ["text_vector"],
                                     "title_field": "title",
                                     "url_field": "url",
